ddccd9571d1d8abc176f73d0c9abc23f60d3260c
Shayan-Rehan777 <159838275+Shayan-meo@users.noreply.github.com>
2026-08-10 02:22:09 +0500

    Supabase

---
+++ suba base querie/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE citizes (
+    name TEXT NOT NULL,
+    country TEXT,
+    population INT,
+    area int 
+    )
+    
+CREATE TABLE Products (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Category VARCHAR(50),
+    Price INT,
+    Stock INT
+    )
+
+CREATE  citizes (
+    name TEXT NOT NULL,
+    country TEXT,
+    population INT,
+    area int 
+);
+    
---
+++ suba base querie/seed.sql
@@ -0,0 +1,25 @@
+INSERT INTO Products (ProductID, ProductName, Category, Price, Stock)
+VALUES
+(1, 'Laptop', 'Electronics', 85000, 10),
+(2, 'Wireless Mouse', 'Accessories', 2500, 25),
+(3, 'Keyboard', 'Accessories', 4500, 15),
+(4, 'Smartphone', 'Electronics', 65000, 8),
+(5, 'Headphones', 'Audio', 7500, 20),
+(6, 'USB Cable', 'Accessories', 1200, 50),
+(7, 'Smart Watch', 'Wearables', 15000, 12),
+(8, 'Bluetooth Speaker', 'Audio', 5500, 18),
+(9, 'Monitor', 'Electronics', 32000, 7),
+(10, 'Power Bank', 'Accessories', 4000, 30);
+
+INSERT INTO citizes (name, country, population, area )
+VALUES
+( 'Laptop', 'Pakistan', 85000, 10),
+( 'Wireless Mouse', 'Indian', 2500, 25),
+( 'Keyboard', 'China', 4500, 15),
+( 'Smartphone', 'USA', 65000, 8),
+( 'Headphones', 'Spain', 7500, 20),
+( 'USB Cable', 'Spain', 1200, 50),
+( 'Smart Watch', 'England', 15000, 12),
+( 'Bluetooth Speaker', 'USA', 5500, 18),
+( 'Monitor', 'China', 32000, 7),
+( 'Power Bank', 'Iran', 4000, 30);
